8d2206f32780d0c293e05aea5bb0c767ebf584ee
KalaHanok <hanokkala123@gmail.com>
2023-04-25 13:59:15 +0530

    sixth commit

--- src/components/Podcast.js
+++ src/components/Podcast.js
@@ -1,10 +1,11 @@
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
+import { useEffect, useState } from "react";
 
 const Podcast = (props) => {
-  let podcasts = props.podcast;
-
+  const [podcasts, setPodcasts] = useState([]);
+  const [file, setFile] = useState("");
   const handleLike = (e) => {
     if (e.target.className === "fa-regular fa-heart") {
       e.target.className = "fa-solid fa-heart";
@@ -12,55 +13,83 @@ const Podcast = (props) => {
       e.target.className = "fa-regular fa-heart";
     }
   };
-  const handlePlay = (e) => {
+  const handlePlay = (e, podcast) => {
     if (e.target.className === "fa-solid fa-play") {
       e.target.className = "fa-solid fa-pause";
     } else {
       e.target.className = "fa-solid fa-play";
     }
+
+    // Api calls
+    console.log(podcast);
+    const res = fetch(`http://127.0.0.1:8000/media/${podcast.PodcastFile}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Token " + localStorage.getItem("token"),
+      },
+    }).then(async (res) => {
+      setFile(await res.text());
+    });
   };
 
-  const res = fetch("http://127.0.0.1:8000/api/audiopodcasts/", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Token " + localStorage.getItem("token"),
-    },
-  }).then(async (data) => {
-    podcasts = await data.json();
-    console.log(podcasts);
-    return (
-      <div className="container">
-        <div className="box">
-          <Row xs={1} md={2} className="g-4  container">
-            {podcasts.map((podcast, index) => (
-              <Col key={index}>
-                <Card>
-                  <Card.Body>
-                    <Card.Title>{podcast.title}</Card.Title>
-                    <Card.Text>{podcast.desc}</Card.Text>
-                  </Card.Body>
-                  <div className="icon">
+  useEffect(() => {
+    const fetchPods = async () => {
+      const res = fetch("http://127.0.0.1:8000/api/audiopodcasts/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Token " + localStorage.getItem("token"),
+        },
+      });
+
+      const data = await (await res).json();
+      const pods = JSON.parse(data);
+      setPodcasts(pods);
+      console.log(pods);
+    };
+
+    fetchPods();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="box">
+        <Row xs={1} md={2} className="g-4  container">
+          {podcasts.map((podcast, index) => (
+            <Col key={index} id={index}>
+              <Card>
+                <Card.Body>
+                  {/* {console.log(podcast)} */}
+                  <Card.Title>{podcast.PodcastName}</Card.Title>
+                  <Card.Text>{podcast.PodcastDescription}</Card.Text>
+                </Card.Body>
+                <div className="icon">
                     <i
-                      onClick={handlePlay}
-                      style={{ margin: "4px", cursor: "pointer" }}
                       className="fa-solid fa-play"
-                    ></i>
-                    <i
-                      onClick={handleLike}
                       style={{ margin: "4px", cursor: "pointer" }}
-                      className="fa-regular fa-heart"
-                    ></i>
-                  </div>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </div>
+                      onClick={(e) => handlePlay(e, podcast)}
+                      id="play"
+                    >
+
+                  <audio src={file} >
+                  </audio>
+
+                    </i>
+
+                  <i
+                    onClick={handleLike}
+                    style={{ margin: "4px", cursor: "pointer" }}
+                    className="fa-regular fa-heart"
+                  ></i>
+                </div>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </div>
-    );
-  });
-  console.log(podcasts);
+    </div>
+  );
 };
 
 export default Podcast;
